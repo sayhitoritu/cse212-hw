330c8ffe941d7183ba28ecf574e4acab964f5219
Mayur Uthale <sayhitomayur@gmail.com>
2026-06-29 19:29:57 +0530

    add

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -10,7 +10,26 @@ public static class ArraySelector
     }
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
+{
+    int[] result = new int[select.Length];
+
+    int index1 = 0;
+    int index2 = 0;
+
+    for (int i = 0; i < select.Length; i++)
     {
-        return [];
+        if (select[i] == 1)
+        {
+            result[i] = list1[index1];
+            index1++;
+        }
+        else
+        {
+            result[i] = list2[index2];
+            index2++;
+        }
     }
+
+    return result;
+}
 }
