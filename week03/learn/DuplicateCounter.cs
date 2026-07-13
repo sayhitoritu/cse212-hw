f00e894ad6142df888a5ecd7d3ca9280e34bdcd2
SayHiToRitu <sayhitoritu94@gmail.com>
2026-07-13 12:33:53 +0530

    Completed Week 03 Learning Activities

--- week03/learn/DuplicateCounter.cs
+++ week03/learn/DuplicateCounter.cs
@@ -24,7 +24,21 @@
 
     private static int CountDuplicates(int[] data)
     {
-        // Add code here.
-        return 0;
+        HashSet<int> unique = new HashSet<int>();
+        int duplicates = 0;
+
+        foreach (int number in data)
+        {
+            if (unique.Contains(number))
+            {
+                duplicates++;
+            }
+            else
+            {
+                unique.Add(number);
+            }
+        }
+
+        return duplicates;
     }
 }
